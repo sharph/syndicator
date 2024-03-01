0743bf8280a625fa50480f8f47f2d830f0c3f75b
Sharp Hall <sharp@sharphall.org>
2024-03-01 16:53:47 -0500

    Display errors on login form

--- syndicator-web/src/routes/login/+page.server.ts
+++ syndicator-web/src/routes/login/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import { getApi } from '$lib/api';
+import { getApi, APIUserError } from '$lib/api';
 import { invalidate } from '$app/navigation';
 
 export const actions = {
@@ -12,7 +12,25 @@ export const actions = {
                 data.get('email') as string,
                 data.get('password') as string
             );
-        } catch (e) { }
+        } catch (e) {
+            if (e instanceof APIUserError) {
+                try {
+                    const body = await e.response.json();
+                    return {
+                        success: false,
+                        body
+                    };
+                } catch { }
+                return {
+                    success: false,
+                    message: e.message,
+                };
+            }
+            return {
+                success: false,
+                message: (e as any).message,
+            };
+        }
         if (user) {
             redirect(302, '/');
         }
--- syndicator-web/src/routes/login/+page.svelte
+++ syndicator-web/src/routes/login/+page.svelte
@@ -2,10 +2,45 @@
 	import { invalidate } from "$app/navigation";
 	import { onMount } from "svelte";
 
+	export let form;
+
 	onMount(() => {
 		invalidate('app:user');
 	});
 </script>
+
+{#if form?.success === false}
+	<div class="ml-auto mr-auto max-w-4xl p-5">
+		{#if form.body?.error}
+			<div
+				class="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
+				role="alert"
+			>
+				{form.body.error}
+			</div>
+		{/if}
+		{#if form.body?.errors}
+			{#each form.body.errors as error}
+				<div
+					class="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
+					role="alert"
+				>
+					<p>{error}</p>
+				</div>
+			{/each}
+		{/if}
+		{#if form.message}
+			<div
+				class="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
+				role="alert"
+			>
+				<p>{form.message}</p>
+			</div>
+		{/if}
+	</div>
+{/if}
+
+
 
 <div class="ml-auto mr-auto max-w-4xl">
 	<form class="flex flex-col gap-4 p-5" method="post">
