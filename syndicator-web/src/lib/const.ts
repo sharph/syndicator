40bfcf0ae0a574b106ab33a6135617f55e4cf2c0
Sharp Hall <sharp@sharphall.org>
2024-03-05 11:57:36 -0500

    Add footer

--- syndicator-web/src/lib/const.ts
+++ syndicator-web/src/lib/const.ts
@@ -3,4 +3,9 @@ import { env } from "$env/dynamic/public"
 
 export const API_ROOT = env.PUBLIC_API_ROOT || (browser ? "http://localhost:8000" : "http://127.0.0.1:8000");
 
+export const GIT_HASH = env.PUBLIC_GIT_HASH || null;
+export const GIT_HASH_SHORT = GIT_HASH ? GIT_HASH.substring(0, 7) : null;
+
 export const APP_TITLE = "syndicator";
+export const SHOW_VERSION = true;
+export const GITHUB_REPO_LINK = GIT_HASH ? `https://github.com/sharph/syndicator/tree/${GIT_HASH}` : 'https://github.com/sharph/syndicator/';
--- syndicator-web/src/routes/+layout.svelte
+++ syndicator-web/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
-<script>
+<script lang="ts">
 	import '../app.pcss';
-	import { APP_TITLE } from '$lib/const';
+	import { APP_TITLE, SHOW_VERSION, GITHUB_REPO_LINK, GIT_HASH_SHORT, API_ROOT } from '$lib/const';
 	import {
 		AppShell,
 		AppRail,
@@ -9,6 +9,8 @@
 		TabGroup,
 		TabAnchor
 	} from '@skeletonlabs/skeleton';
+	import type { AfterNavigate } from '@sveltejs/kit';
+	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Icon from 'svelte-icons-pack/Icon.svelte';
 	import FaSolidRss from 'svelte-icons-pack/fa/FaSolidRss';
@@ -19,6 +21,16 @@
 	import FaSolidSignOutAlt from 'svelte-icons-pack/fa/FaSolidSignOutAlt';
 	import FaSolidUserPlus from 'svelte-icons-pack/fa/FaSolidUserPlus';
 	export let data;
+
+	// reset scroll position fix
+
+	afterNavigate((params: AfterNavigate) => {
+		const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
+		const elemPage = document.querySelector('#page');
+		if (isNewPage && elemPage !== null) {
+			elemPage.scrollTop = 0;
+		}
+	});
 </script>
 
 <svelte:head>
@@ -36,52 +48,52 @@
 			flex="flex-1 lg:flex-none"
 			rounded=""
 			border=""
-			class="bg-surface-100-800-token w-full block md:hidden"
+			class="bg-surface-100-800-token block w-full md:hidden"
 		>
 			{#if data.user}
-			<TabAnchor href="/" selected={$page.url.pathname === '/'}>
-				<svelte:fragment slot="lead">
-					<Icon src={FaNewspaper} />
-				</svelte:fragment>
-				<span>Articles</span>
-			</TabAnchor>
-			<TabAnchor href="/favorites" selected={$page.url.pathname === '/favorites'}>
-				<svelte:fragment slot="lead">
-					<Icon src={FaSolidHeart} />
-				</svelte:fragment>
-				<span>Favorites</span>
-			</TabAnchor>
-			<TabAnchor href="/subscriptions" selected={$page.url.pathname === '/subscriptions'}>
-				<svelte:fragment slot="lead">
-					<Icon src={FaSolidRss} />
-				</svelte:fragment>
-				<span>Subscriptions</span>
-			</TabAnchor>
-			<TabAnchor href="/account" selected={$page.url.pathname === '/account'}>
-				<svelte:fragment slot="lead">
-					<Icon src={FaSolidUser} />
-				</svelte:fragment>
-				<span>Account</span>
-			</TabAnchor>
-			<TabAnchor href="/logout">
-				<svelte:fragment slot="lead">
-					<Icon src={FaSolidSignOutAlt} />
-				</svelte:fragment>
-				<span>Logout</span>
-			</TabAnchor>
+				<TabAnchor href="/" selected={$page.url.pathname === '/'}>
+					<svelte:fragment slot="lead">
+						<Icon src={FaNewspaper} />
+					</svelte:fragment>
+					<span>Articles</span>
+				</TabAnchor>
+				<TabAnchor href="/favorites" selected={$page.url.pathname === '/favorites'}>
+					<svelte:fragment slot="lead">
+						<Icon src={FaSolidHeart} />
+					</svelte:fragment>
+					<span>Favorites</span>
+				</TabAnchor>
+				<TabAnchor href="/subscriptions" selected={$page.url.pathname === '/subscriptions'}>
+					<svelte:fragment slot="lead">
+						<Icon src={FaSolidRss} />
+					</svelte:fragment>
+					<span>Subscriptions</span>
+				</TabAnchor>
+				<TabAnchor href="/account" selected={$page.url.pathname === '/account'}>
+					<svelte:fragment slot="lead">
+						<Icon src={FaSolidUser} />
+					</svelte:fragment>
+					<span>Account</span>
+				</TabAnchor>
+				<TabAnchor href="/logout">
+					<svelte:fragment slot="lead">
+						<Icon src={FaSolidSignOutAlt} />
+					</svelte:fragment>
+					<span>Logout</span>
+				</TabAnchor>
 			{:else}
-			<TabAnchor href="/login">
-				<svelte:fragment slot="lead">
-					<Icon src={FaSolidSignInAlt} />
-				</svelte:fragment>
-				<span>Login</span>
-			</TabAnchor>
-			<TabAnchor href="/register">
-				<svelte:fragment slot="lead">
-					<Icon src={FaSolidUserPlus} />
-				</svelte:fragment>
-				<span>Register</span>
-			</TabAnchor>
+				<TabAnchor href="/login">
+					<svelte:fragment slot="lead">
+						<Icon src={FaSolidSignInAlt} />
+					</svelte:fragment>
+					<span>Login</span>
+				</TabAnchor>
+				<TabAnchor href="/register">
+					<svelte:fragment slot="lead">
+						<Icon src={FaSolidUserPlus} />
+					</svelte:fragment>
+					<span>Register</span>
+				</TabAnchor>
 			{/if}
 		</TabGroup>
 	</svelte:fragment>
@@ -135,6 +147,21 @@
 		</AppRail>
 	</svelte:fragment>
 	<slot />
+	<svelte:fragment slot="footer">
+		{#if SHOW_VERSION}
+			<div class="hidden md:block">
+				<AppBar
+					gridColumns="grid-cols-1"
+					slotDefault="place-self-start"
+					slotTrail="place-content-end"
+				>
+					<a class="me-3" href={GITHUB_REPO_LINK}>syndicator/{GIT_HASH_SHORT || 'dev'}</a>
+					<a class="me-3" href={`${API_ROOT}/docs`}>API</a>
+					<a class="me-3" href="https://github.com/sharph/syndicator/blob/main/LICENSE.txt">AGPLv3</a>
+				</AppBar>
+			</div>
+		{/if}
+	</svelte:fragment>
 </AppShell>
 
 <style type="text/css">
