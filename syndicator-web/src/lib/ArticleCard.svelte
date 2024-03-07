e864f2eaead9064dd0e091d628b08b311b2a82be
Sharp Hall <sharp@sharphall.org>
2024-03-07 14:14:48 -0500

    Show nice pub dates

--- syndicator-web/src/lib/ArticleCard.svelte
+++ syndicator-web/src/lib/ArticleCard.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-  import Icon from 'svelte-icons-pack/Icon.svelte';
-  import FaSolidHeart from 'svelte-icons-pack/fa/FaSolidHeart';
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import FaSolidHeart from 'svelte-icons-pack/fa/FaSolidHeart';
+	import { DateTime } from 'luxon';
 	import type { components } from './apiTypes';
 	import { getApi } from './api';
 	export let article: components['schemas']['ItemSchema'];
 
 	const api = getApi(fetch);
+	const pub_date = DateTime.fromISO(article.pub_date);
 
 	async function click() {
 		await api.click(`${article.feed.path}/${article.path}`);
@@ -25,13 +27,16 @@
 
 <div class="card ml-auto mr-auto max-w-4xl shadow-xl">
 	<a target="_blank" href={article.link} on:click={click}>
-		<div class="grid grid-cols-1 md:grid-cols-3 w-full bg-surface-50 cardbody border-b-2" class:clicked={article.clicked}>
+		<div
+			class="bg-surface-50 cardbody grid w-full grid-cols-1 border-b-2 md:grid-cols-3"
+			class:clicked={article.clicked}
+		>
 			<div class="m-4 md:col-span-2">
-				<h2 class="text-2xl md:text-4xl font-bold">
+				<h2 class="text-2xl font-bold md:text-4xl">
 					<a target="_blank" href={article.link}>{article.title}</a>
 				</h2>
 				<p class="text-xl md:text-2xl">{article.feed.title}</p>
-				<p class="text-sm">{article.pub_date}</p>
+				<p class="text-sm">{pub_date.toLocaleString(DateTime.DATETIME_MED)}</p>
 				<p>{article.description}</p>
 			</div>
 			<div class="m-4 md:col-span-1">
@@ -52,28 +57,31 @@
 	.card {
 		transition: transform 0.2s;
 	}
-  .card .cardbody {
-    transition: background-color 0.2s;
-  }
+	.card .cardbody {
+		transition: background-color 0.2s;
+	}
 	.card:hover {
 		transform: scale(1.01);
 	}
-  .card .cardbody:hover {
-    background-color: rgb(var(--color-primary-50));
-  }
-	.clicked {
-    background-color: rgb(var(--color-primary-200));
+	.card .cardbody:hover {
+		background-color: rgb(var(--color-primary-50));
 	}
-  .favorite {
-    transition: opacity 0.2s, transform 0.2s, color 0.2s;
-    opacity: 0.3;
-    transform: scale(0.8);
-  }
-  .favorite.favorited {
-    opacity: 1;
-    transform: scale(1);
-  }
-  .favorite:hover {
-    transform: scale(1.5);
-  }
+	.clicked {
+		background-color: rgb(var(--color-primary-200));
+	}
+	.favorite {
+		transition:
+			opacity 0.2s,
+			transform 0.2s,
+			color 0.2s;
+		opacity: 0.3;
+		transform: scale(0.8);
+	}
+	.favorite.favorited {
+		opacity: 1;
+		transform: scale(1);
+	}
+	.favorite:hover {
+		transform: scale(1.5);
+	}
 </style>
